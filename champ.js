74312ec6413024ba066724609bd1af818347d440
Vasco Gomes <ovascogomes@gmail.com>
2017-06-04 17:38:12 +0100

    Added random quote to all tweets

--- champ.js
+++ champ.js
@@ -1,12 +1,55 @@
 "use strict";
 
 var twit   = require("twit");
-var tokens = require("./twitter_config.js");
-
-console.log(tokens);
+var tokens = require("./tokens.js");
+var config = require("./config.js");
 
 var api = new twit(tokens);
+var hashtags = "#" +config.hashtags.join(" #");
 
-api.post("statuses/update", { status: "cena demo" }, function(err, data, response) {
-	console.log(data);
-});
+/**
+ * Main method, responsible for the actual API call.
+ */
+function tweet() {
+	var params = {
+		status: getRandomQuote() + " " + hashtags
+	}
+
+	api.post("statuses/update", params, function(err, data, response) {
+		if (err || response.statusCode !== 200) {
+			console.error("Unable to post to Twitter.", err);
+			return;
+		}
+
+		console.log("Tweet posted successfully at " + data.created_at + ".");
+		console.log("Url: ", "https://twitter.com/" + data.user.screen_name + "/status/" + data.id_str);
+	});
+}
+
+/**
+ * Gets a random quote from config.js according to the probability specified.
+ */
+function getRandomQuote() {
+	var random = Math.random();
+	var noQuoteProb = 1 - config.quote_probability;
+
+	if (random < (noQuoteProb / 2)) {
+		return "AND HIS NAME IS JOHN CENA!";
+	}
+	else if (random < noQuoteProb) {
+		return "THE CHAMP IS HERE!";
+	}
+	else {
+		return config.quotes[getRandom(0, config.quotes.length - 1)];
+	}
+}
+
+/**
+ * Auxiliar, from Mozilla's JS examples. Gets a random int between min and max (both inclusive).
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ */
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+tweet();
---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	tweet_interval: 3600,
+	quote_probability: "0.3",
+	quotes: [
+		"If you want some, come get some.",
+		"Live fast, fight hard, no regrets!",
+		"I don't have any elaborate uniforms. I wear a t-shirt, a pair of sneakers and some shorts.",
+		"If you don't learn from your mistakes, then they become regrets.",
+		"There are moments where it gets so hectic that time becomes a blur. Keep calm, and never give up.",
+		"When I hear people boo, that just makes me want to go out there and work harder.",
+		"I try to tackle every day the best I can to make life as productive and positive as I can.",
+		"Hustle, loyalty, respect.",
+		"Watching wrestling with my old man was the equivalent of playing catch with your dad.",
+		"You want The Next Big Thing? Let me take my pants off!",
+		"Whether you like me or you don't, I still dig showing up for work.",
+		"I fear nothing and I regret less.",
+		"This is my life, this is my passion, this is all I got.",
+		"Thank you for letting idiots like me come out and live this wonderful wonderful dream.",
+		"I take pride in the fact that a lot of little kids dig what I do.",
+		"You smell like crap so I know you don't like soap.",
+		"When you see me on TV, that's about as close to my real personality as you can get.",
+		"My home is at the WWE. I truly do love performing. It's truly where I belong.",
+		"I don't play a character. It's totally just an extension of myself.",
+		"There are those who talk about it and those who make a difference. Ask yourself: which one am I?",
+		"I'm fine with being beat-up and pushed around, but I'm really scared of heights and ledges.",
+		"Everyday life is a struggle, with the biggest victory being the chance to keep struggling.",
+		"Always work as hard as you can. Pay it forward. Be a good human being.",
+		"Plans always change, but a goal with no plan is just a dream. Plan to turn dreams to reality.",
+		"Focus on one task at a time, take it one day at a time."
+	],
+	hashtags: [
+		"johncena",
+		"wwe",
+		"champ"
+	]
+}
